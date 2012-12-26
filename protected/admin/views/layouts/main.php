9913a76bbc7723af021586a5792123ff6f1f61e9
smarteng <smarteng@qq.com>
2012-12-26 22:09:33 +0800

    aldert to be iframe

--- protected/admin/views/layouts/main.php
+++ protected/admin/views/layouts/main.php
@@ -8,7 +8,7 @@
     <!-- Le styles -->
     <link href="/admin/css/bootstrap.css" rel="stylesheet">
     <link href="/admin/css/bootstrap-responsive.css" rel="stylesheet">
-	  <link href="/admin/css/admin.css" rel="stylesheet">
+    <link href="/admin/css/admincontent.css" rel="stylesheet">
 
     <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
@@ -17,9 +17,8 @@
   </head>
 
   <body>
-    <div class="span9" id="maincontent">
       <?php echo $content; ?>
-    </div><!--/span9-->
+
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="/admin/js/bootstrap.js"></script>
     <script type="text/javascript">
