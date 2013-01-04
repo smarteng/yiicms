299478b4318a7f037f628c58f9d24239fda62e25
smarteng <smarteng@qq.com>
2013-01-04 18:26:17 +0800

    查看删除提示
    删除提示，在边距小的情况不能显示

--- protected/admin/views/site/welcome.php
+++ protected/admin/views/site/welcome.php
@@ -80,13 +80,16 @@ $this->widget('bootstrap.TbGridView', array(
       e.preventDefault();
       $(this).tab('show');
     });
-  $('#buttonStateful').click(function() {
-      var btn = $(this);
-      btn.button('loading'); // call the loading function
-      setTimeout(function() {
-          btn.button('reset'); // call the reset function
-      }, 500);
-  });
+    $('#buttonStateful').click(function() {
+        var btn = $(this);
+        btn.button('loading'); // call the loading function
+        setTimeout(function() {
+            btn.button('reset'); // call the reset function
+        }, 500);
+    });
+    $('body').tooltip({
+            selector: '[rel=tooltip]'
+    });
   });
 </script>
 </div><!--/row-->
